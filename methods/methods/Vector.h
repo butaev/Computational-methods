f6c5941f48d00f664385dadaf3b4ff247cf77fde
butaev <butaev-raul@mail.ru>
2018-06-25 19:36:44 +0700

    Update Vector.h

--- methods/methods/Vector.h
+++ methods/methods/Vector.h
@@ -15,18 +15,17 @@ public:
         double& operator[](const int i);
         friend Vector operator+(const Vector a, const Vector b);
         Vector& operator=(const Vector& a);
-		void Swap(const int i, const int j);
-		//////////////
-		friend std::ostream& operator<<(std::ostream& os, const Vector& a);
-		friend std::istream& operator>>(std::istream& is, Vector& a);
-		friend double operator*(const Vector& a, const Vector& b);
-		friend Vector operator*(const double a, Vector& b);
-		friend Vector operator-(const Vector& a, const Vector& b);
-		//////////////
+	void Swap(const int i, const int j);
+	//////////////
+	friend std::ostream& operator<<(std::ostream& os, const Vector& a);
+	friend std::istream& operator>>(std::istream& is, Vector& a);
+	friend double operator*(const Vector& a, const Vector& b);
+	friend Vector operator*(const double a, Vector& b);
+	friend Vector operator-(const Vector& a, const Vector& b);
+	//////////////
 
         int Length() const;
-		double Norm() const;
-
+	double Norm() const;
 
         ~Vector();
 };
